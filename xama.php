5793b8b71534ba091216da206639762927d9f26b
badasswp <badasswpdev@gmail.com>
2023-11-03 12:00:17 +0100

    chore: set up plugin entry point

---
+++ xama.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Plugin Name: Xama
+ * Plugin URI:  https://github.com/chigozieorunta/xama
+ * Description: A simple WP plugin to help you set up Quizzes.
+ * Version:     1.0.0
+ * Author:      Chigozie Orunta
+ * Author URI:  https://chigozieorunta.com
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: xama
+ * Domain Path: /languages
+ *
+ * @package Xama
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'XAMA_PLUGIN_AUTOLOAD', __DIR__ . '/vendor/autoload.php' );
+
+/**
+ * Checks to see if Composer's autoload file can load.
+ *
+ * @return bool True or false if it can.
+ */
+function xama_can_autoload() {
+	if ( ! file_exists( XAMA_PLUGIN_AUTOLOAD ) ) {
+		error_log(
+			sprintf(
+				esc_html__( 'Fatal Error: Composer not setup in %', 'xama' ),
+				__DIR__
+			)
+		);
+		return false;
+	}
+	// Require autoload.
+	require_once XAMA_PLUGIN_AUTOLOAD;
+
+	return true;
+}
+
+/**
+ * Generates autoload notices.
+ *
+ * @return void
+ */
+function xama_autoload_notice() {
+	$class   = 'notice notice-error';
+	$message = 'Error: Composer not setup! Please run $ Composer install in the Xama plugin directory.';
+
+	// Display notice.
+	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+}
+
+/**
+ * Run plugin.
+ *
+ * @return void
+ */
+function xama_run() {
+	if ( xama_can_autoload() ) {
+		require_once __DIR__ . '/inc/Helpers/functions.php';
+		( \Xama\Plugin::get_instance() )->run();
+	} else {
+		add_action( 'admin_notices', 'xama_autoload_notice' );
+	}
+}
+
+xama_run();
