77c6909f665e8932b87167482f44e5d6c3ee0104
badasswp <badasswpdev@gmail.com>
2023-11-04 00:24:19 +0100

    feat: add Menu service to Container list

---
+++ inc/Core/Container.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Container Class.
+ *
+ * This class is responsible for managing and registering
+ * various services used by the Xama plugin. Services are
+ * registered to bind custom logic to WordPress hooks.
+ *
+ * @package Xama
+ */
+
+namespace Xama\Core;
+
+use Xama\Services\Menu;
+use Xama\Interfaces\Registrable;
+
+/**
+ * Container class.
+ */
+final class Container implements Registrable {
+	/**
+	 * Instantiated services.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var array
+	 */
+	public static array $services = [];
+
+	/**
+	 * Initialize services in the constructor.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		self::$services = [
+			Menu::class,
+		];
+	}
+
+	/**
+	 * Register services (bind logic to WP).
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+		foreach ( Container::$services as $service ) {
+			( $service::get_instance() )->register();
+		}
+	}
+}
