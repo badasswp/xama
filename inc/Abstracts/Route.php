dce930f6c0a138b924e074df53a078a5105f06c0
badasswp <badasswpdev@gmail.com>
2023-11-04 12:10:58 +0100

    feat: add Route abstract class to act as base layer for routes

---
+++ inc/Abstracts/Route.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Route abstraction.
+ *
+ * This abstract class defines a foundation for creating
+ * route classes which act as WP REST end points for frontend
+ * applications.
+ *
+ * @package Xama
+ */
+
+namespace Xama\Abstracts;
+
+use Xama\Interfaces\Route as RouteInterface;
+
+abstract class Route implements RouteInterface {
+	/**
+	 * Set up.
+	 */
+	public function __construct() {
+		register_rest_route(
+			'xama/v1',
+			'/' . $this->endpoint,
+			[
+				'methods'             => $this->method,
+				'callback'            => [ $this, 'callback' ],
+				'permission_callback' => $this->is_user_permissible() ? '__return_true' : '__return_false',
+			]
+		);
+	}
+
+	/**
+	 * REST Callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 * @return WP_REST_Response
+	 */
+	public function callback( $request ): \WP_REST_Response {
+		$this->request = $request;
+
+		return $this->request();
+	}
+
+	/**
+	 * Request Callback.
+	 *
+	 * @return WP_REST_Response
+	 */
+	abstract public function request(): \WP_REST_Response;
+
+	/**
+	 * Response Callback.
+	 *
+	 * @return WP_REST_Response
+	 */
+	abstract public function response(): \WP_REST_Response;
+
+	/**
+	 * Permissions callback for endpoints.
+	 *
+	 * @return bool
+	 */
+	abstract public function is_user_permissible(): bool;
+}
