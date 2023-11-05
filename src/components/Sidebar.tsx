a4b9deca683812276f0878d148a65fe0c9f591b8
badasswp <badasswpdev@gmail.com>
2023-11-05 10:26:04 +0100

    feat: add Sidebar component to hold title and content

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,21 @@
+import Title from './Title';
+import Content from './Content';
+
+/**
+ * Sidebar Component
+ *
+ * This component is responsible for rendering a fixed sidebar
+ * section. It contains a title and content.
+ *
+ * @returns {JSX.Element}  The rendered Sidebar component.
+ */
+const Sidebar = () => {
+  return (
+    <section>
+      <Title />
+      <Content />
+    </section>
+  )
+}
+
+export default Sidebar;
