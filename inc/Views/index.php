369e71bd08d96dc56c84f74bd7ab77dec99253b3
badasswp <badasswpdev@gmail.com>
2023-11-05 12:30:42 +0100

    fix: update src path and DOM id

--- inc/Views/index.php
+++ inc/Views/index.php
@@ -7,7 +7,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 	<script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
 	<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
-	<script type="text/javascript" src="http://localhost:2048/wp-content/plugins/xama/dist/app.js"></script>
+	<script type="text/javascript" src="http://localhost:2468/wp-content/plugins/xama/dist/app.js"></script>
 	<style type="text/css">
 		body {
 			font-family: 'Inter', 'sans-serif';
@@ -18,6 +18,6 @@
 	</style>
 </head>
 <body id="<?php global $post; echo $post->ID; ?>">
-	<div id="app"></div>
+	<div id="xama"></div>
 </body>
 </html>
