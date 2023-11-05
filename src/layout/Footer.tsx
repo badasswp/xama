f94e1a82351a378189163157db80d7c88e51d3d0
badasswp <badasswpdev@gmail.com>
2023-11-05 10:04:52 +0100

    feat: add Footer component

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,15 @@
+/**
+ * Footer Component
+ *
+ * This component defines the footer of the layout structure,
+ * and displays across all views.
+ *
+ * @returns {JSX.Element}  The rendered Footer component.
+ */
+const Footer = () => {
+  return (
+    <></>
+  )
+}
+
+export default Footer;
