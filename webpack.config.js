8fa738e9b32c1c26ae2cfc9ce97c1ba0c5c3ba98
badasswp <badasswpdev@gmail.com>
2023-11-05 10:53:34 +0100

    chore: add sass-loader plugin for CSS compiles

--- webpack.config.js
+++ webpack.config.js
@@ -53,8 +53,8 @@ module.exports = {
         ],
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
