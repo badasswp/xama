67a41fcec84d5617b386d8f895e142a7dae327e7
badasswp <badasswpdev@gmail.com>
2023-11-05 09:27:00 +0100

    feat: add Middleware abstraction as base layer for all middlewares

---
+++ inc/Abstracts/Middleware.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Middleware abstraction.
+ *
+ * This abstract class serves as the foundation for creating
+ * middleware classes responsible for authentication. It defines
+ * common methods that derived classes should implement.
+ *
+ * @package Xama
+ */
+
+namespace Xama\Abstracts;
+
+/**
+ * Middleware class.
+ */
+abstract class Middleware implements \Xama\Interfaces\Middleware {
+	/**
+	 * Authenticate user.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	abstract public function authenticate(): void;
+
+	/**
+	 * Redirect user.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function redirect(): void {
+		$this->logout();
+
+		$page = get_page_by_path( $this->redirect_page, OBJECT, 'page' );
+		wp_redirect( get_permalink( $page->ID ) );
+		exit;
+	}
+
+	/**
+	 * On Login.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	abstract public function login(): void;
+
+	/**
+	 * On Logout.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	abstract public function logout(): void;
+}
