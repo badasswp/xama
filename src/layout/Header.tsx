d1894ebb397f0387d8d66cae2c2a7b557e39b749
badasswp <badasswpdev@gmail.com>
2023-11-05 10:04:37 +0100

    feat: add Header component

---
+++ src/layout/Header.tsx
@@ -0,0 +1,16 @@
+/**
+ * Header Component
+ *
+ * This component defines the header of the layout structure,
+ * and typically holds the navigation component which displays
+ * across all views.
+ *
+ * @returns {JSX.Element}  The rendered Header component.
+ */
+const Header = () => {
+  return (
+    <></>
+  )
+}
+
+export default Header;
