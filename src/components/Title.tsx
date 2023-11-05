ade5a97301cb0e13986f711cda83ebe53b0896ff
badasswp <badasswpdev@gmail.com>
2023-11-05 10:16:55 +0100

    feat: add Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,15 @@
+/**
+ * Title Component
+ *
+ * This component is responsible for the big, bold caption displayed
+ * on the frontend.
+ *
+ * @returns {JSX.Element}  The rendered Title component.
+ */
+const Title = () => {
+  return (
+    <h1></h1>
+  )
+}
+
+export default Title;
