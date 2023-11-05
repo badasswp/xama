79491c0e86aa42d44485920163ce62e5769ba30b
badasswp <badasswpdev@gmail.com>
2023-11-05 10:33:34 +0100

    feat: add Content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,15 @@
+/**
+ * Content Component
+ *
+ * This component represents the content of the post. It
+ * may hold text or/and images.
+ *
+ * @returns {JSX.Element}  The rendered Content component.
+ */
+const Content = () => {
+  return (
+    <div id="content"></div>
+  )
+}
+
+export default Content;
