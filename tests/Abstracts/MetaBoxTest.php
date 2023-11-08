5e836e5b737e4ea626d297df04f4a469f7e2aa50
badasswp <badasswpdev@gmail.com>
2023-11-08 09:16:19 +0100

    test: set up MetaBox abstract test class

---
+++ tests/Abstracts/MetaBoxTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Xama\Tests\Abstracts;
+
+use stdClass;
+use Xama\Core\Settings;
+use Xama\Abstracts\MetaBox;
+use WP_Mock\Tools\TestCase;
+
+/**
+ * @covers MetaBox
+ */
+class MetaBoxTest extends TestCase {
+	private $metabox;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		ConcreteMetaBox::$name = 'xama_mb_metabox';
+		$this->metabox         = new ConcreteMetaBox();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+}
+
+class ConcreteMetaBox extends MetaBox {
+	public function get_heading(): string {
+		return 'MetaBox Heading';
+	}
+
+	public function get_post_type(): string {
+		return 'xama_quiz';
+	}
+
+	public function get_position(): string {
+		return 'advanced';
+	}
+
+	public function get_priority(): string {
+		return 'default';
+	}
+
+	public function get_metabox_callback( $post ): void {}
+	public function save_meta_box( $post_id, $post ): void {}
+}
