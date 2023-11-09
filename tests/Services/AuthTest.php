14f4b67a1858062e8fc4de85bca8afadaf77008f
badasswp <badasswpdev@gmail.com>
2023-11-09 21:57:55 +0100

    test: test that Auth service registers hooks

---
+++ tests/Services/AuthTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Xama\Tests\Services;
+
+use stdClass;
+use Xama\Core\Settings;
+use Xama\Services\Auth;
+use WP_Mock\Tools\TestCase;
+
+/**
+ * @covers Auth
+ */
+class AuthTest extends TestCase {
+	private $auth;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		$this->auth = new Auth();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_register() {
+		\WP_Mock::expectActionAdded( 'wp_login', [ $this->auth, 'register_auth_login' ] );
+		\WP_Mock::expectActionAdded( 'wp_logout', [ $this->auth, 'register_auth_logout' ] );
+		\WP_Mock::expectActionAdded( 'template_redirect', [ $this->auth, 'register_auth_middleware' ] );
+
+		$this->auth->register();
+
+		$this->assertConditionsMet();
+	}
+}
