06bb771ac0442ea7d831ea710f7645c2d5967eae
badasswp <badasswpdev@gmail.com>
2023-11-09 22:04:04 +0100

    test: test that Boot service registers hooks

---
+++ tests/Services/BootTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Xama\Tests\Services;
+
+use stdClass;
+use Xama\Core\Settings;
+use Xama\Services\Boot;
+use WP_Mock\Tools\TestCase;
+
+/**
+ * @covers Boot
+ */
+class BootTest extends TestCase {
+	private $boot;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		\WP_Mock::userFunction( 'plugin_basename' )
+			->once();
+
+		$this->boot = new Boot();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_register() {
+		\WP_Mock::expectActionAdded( 'init', [ $this->boot, 'register_plugin_domain' ] );
+		\WP_Mock::expectActionAdded( 'init', [ $this->boot, 'register_plugin_role' ] );
+
+		$this->boot->register();
+
+		$this->assertConditionsMet();
+	}
+}
