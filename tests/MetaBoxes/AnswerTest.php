a222c8e9ab6180bb23cfa432f7b9c9532f44c24f
badasswp <badasswpdev@gmail.com>
2023-11-09 21:15:56 +0100

    test: test that Answer metabox callback returns HTML select field with options

--- tests/MetaBoxes/AnswerTest.php
+++ tests/MetaBoxes/AnswerTest.php
@@ -57,4 +57,67 @@ class AnswerTest extends TestCase {
 		$this->assertSame( $priority, '' );
 		$this->assertConditionsMet();
 	}
+
+	public function test_get_metabox_callback() {
+		\WP_Mock::userFunction( 'get_post_meta' )
+			->once()
+			->with( 1, 'xama_answer', true )
+			->andReturn( 1 );
+
+		$i = 1;
+
+		while ( $i < 5 ) {
+			$value = Settings::OPTIONS[ $i ];
+
+			\WP_Mock::userFunction( 'esc_html__' )
+				->with( $value, Settings::DOMAIN )
+				->andReturn( $value );
+
+			++$i;
+		}
+
+		$i = 1;
+
+		while ( $i < 5 ) {
+			\WP_Mock::userFunction( 'esc_attr' )
+				->twice()
+				->with( $i )
+				->andReturn( $i );
+
+			++$i;
+		}
+
+		\WP_Mock::userFunction( 'selected' )
+			->once()
+			->with( 1, 1, false )
+			->andReturn( 'selected' );
+
+		\WP_Mock::userFunction( 'selected' )
+			->times( 3 );
+
+		$post     = new stdClass();
+		$post->ID = 1;
+
+		$this->metabox->options = '';
+
+		$this->metabox->get_metabox_callback( $post );
+
+		$this->expectOutputString(
+			'<select
+				class="widefat"
+				name="xama_answer"
+				style="margin-top: 5px;"
+			><option value="1" selected>
+					A
+				</option><option value="2" >
+					B
+				</option><option value="3" >
+					C
+				</option><option value="4" >
+					D
+				</option></select>'
+		);
+
+		$this->assertConditionsMet();
+	}
 }
