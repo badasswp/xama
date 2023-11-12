30a68f44d073ff58d59df8c854a7dd70ac0c913f
badasswp <badasswpdev@gmail.com>
2023-11-13 00:56:04 +0100

    feat: set up actions for fetching posts

---
+++ src/store/actions.ts
@@ -0,0 +1,34 @@
+/**
+ * Actions for managing state.
+ *
+ * This module defines a set of actions for managing state
+ * changes. It provides a set of actions that can be dispatched
+ * to update the global state object using keys.
+ *
+ * @module actions
+ * @returns {Object}  An action object.
+ */
+
+const actions = {
+  fetchPostRequest: () => {
+    return {
+      type: 'FETCH_POST_REQUEST'
+    }
+  },
+
+  fetchPostSuccess: ( post ) => {
+    return {
+      type: 'FETCH_POST_SUCCESS',
+      payload: post
+    }
+  },
+
+  fetchPostFailure: ( error ) => {
+    return {
+      type: 'FETCH_POST_FAILURE',
+      payload: error
+    }
+  },
+};
+
+export default actions;
