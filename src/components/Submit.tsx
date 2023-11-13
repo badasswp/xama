6938242a5ccf5aae122e42c2a9c469cabe171c6f
badasswp <badasswpdev@gmail.com>
2023-11-13 01:05:28 +0100

    feat: add new Submit button

---
+++ src/components/Submit.tsx
@@ -0,0 +1,15 @@
+/**
+ * Submit Component
+ *
+ * This component is responsible for rendering the submit button
+ * that saves and submits answers for a question.
+ *
+ * @returns {JSX.Element}  The rendered Submit component.
+ */
+const Submit = () => {
+  return (
+    <button type="submit">Submit Answer</button>
+  )
+}
+
+export default Submit;
