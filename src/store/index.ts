98c32871cd44427853c15a609ee11e03d95191c3
badasswp <badasswpdev@gmail.com>
2023-11-13 01:02:44 +0100

    chore: set up redux thunk for middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-export default configureStore(
-  {
-    reducer: {}
-  }
-)
+const store = configureStore( {
+  reducer: reducers,
+  middleware: [ thunk ],
+} );
+
+export default store;
