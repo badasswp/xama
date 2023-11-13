5bd2ec69ca13037b0e34238e28e4b862e6c3a28b
badasswp <badasswpdev@gmail.com>
2023-11-13 01:35:50 +0100

    feat: set up async call to fetch posts, dispatch relevant actions

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Layout from './layout/Layout';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
 
+import actions from './store/actions';
+
 /**
  * App Component
  *
@@ -21,6 +23,46 @@ import Dashboard from './components/Dashboard';
  * @returns {JSX.Element}  The rendered App component.
  */
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    ( async () => {
+      /**
+       * Dispatches the fetch post request action
+       * on entry and sets loading to true.
+       *
+       * @name fetchData
+       * @returns {void}
+       */
+      dispatch( actions.fetchPostRequest() );
+
+      try {
+        const response = await fetch( url );
+        const { data } = await response.json();
+
+        /**
+         * On successful data fetch, dispatches the
+         * fetch post success action with the received data to
+         * update the state and set loading to false.
+         *
+         * @name fetchPostSuccess
+         * @returns {void}
+         */
+        dispatch( actions.fetchPostSuccess( data ) );
+      } catch ( error ) {
+        /**
+         * On error during data fetch, dispatch the
+         * fetch post failure action with the encountered error
+         * to update the state and set loading to false.
+         *
+         * @name fetchPostFailure
+         * @returns {void}
+         */
+        dispatch( actions.fetchPostFailure( error ) );
+      }
+    } )();
+  }, [] );
+
   return (
     <Layout>
       <Sidebar/>
