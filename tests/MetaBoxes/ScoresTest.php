b58a38aae8e6d593874987dcd3463975bcdbce98
badasswp <badasswpdev@gmail.com>
2023-11-16 22:29:01 +0100

    test: add basic tests for Scores metabox

--- tests/MetaBoxes/ScoresTest.php
+++ tests/MetaBoxes/ScoresTest.php
@@ -23,4 +23,39 @@ class ScoresTest extends TestCase {
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
 	}
+
+	public function test_get_name() {
+		$name = Scores::$name;
+
+		$this->assertSame( $name, 'xama_mb_scores' );
+		$this->assertConditionsMet();
+	}
+
+	public function test_get_heading() {
+		$heading = $this->scores->get_heading();
+
+		$this->assertSame( $heading, 'Quiz Scores' );
+		$this->assertConditionsMet();
+	}
+
+	public function test_get_post_type() {
+		$post_type = $this->scores->get_post_type();
+
+		$this->assertSame( $post_type, 'xama_score' );
+		$this->assertConditionsMet();
+	}
+
+	public function test_get_position() {
+		$position = $this->scores->get_position();
+
+		$this->assertSame( $position, '' );
+		$this->assertConditionsMet();
+	}
+
+	public function test_get_priority() {
+		$priority = $this->scores->get_priority();
+
+		$this->assertSame( $priority, 'high' );
+		$this->assertConditionsMet();
+	}
 }
