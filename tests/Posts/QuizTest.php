89ddcccf88262b04e836921d7bf88648066cb4f7
badasswp <badasswpdev@gmail.com>
2023-11-18 19:34:11 +0100

    test: set up basic tests for Quiz post type

--- tests/Posts/QuizTest.php
+++ tests/Posts/QuizTest.php
@@ -22,4 +22,25 @@ class QuizTest extends TestCase {
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
 	}
+
+	public function test_get_name() {
+		$name = Quiz::$name;
+
+		$this->assertSame( $name, 'xama_quiz' );
+		$this->assertConditionsMet();
+	}
+
+	public function test_get_singular_label() {
+		$singular_label = $this->post->get_singular_label();
+
+		$this->assertSame( $singular_label, 'Quiz' );
+		$this->assertConditionsMet();
+	}
+
+	public function test_get_plural_label() {
+		$plural_label = $this->post->get_plural_label();
+
+		$this->assertSame( $plural_label, 'Quizzes' );
+		$this->assertConditionsMet();
+	}
 }
