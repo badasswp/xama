3cec3ac7f759ea5aa2b7416de2875a1f7efd6bfb
badasswp <badasswpdev@gmail.com>
2023-12-01 21:15:08 +0100

    refactor: update js script path with dynamic string

--- inc/Views/index.php
+++ inc/Views/index.php
@@ -7,7 +7,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 	<script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
 	<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
-	<script type="text/javascript" src="http://localhost:2468/wp-content/plugins/xama/dist/app.js"></script>
+	<script type="text/javascript" src="<?php echo plugins_url(); ?>/xama/dist/app.js"></script>
 </head>
 <body>
 	<div id="xama" data-id="<?php global $post; echo $post->ID; ?>" data-url="<?php echo home_url(); ?>" data-user="<?php echo wp_get_current_user()->ID; ?>" data-login="<?php echo wp_get_current_user()->user_login; ?>"></div>
