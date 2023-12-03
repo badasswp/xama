5c917604ba7fc0942e33bebcdf7eb956845cdd49
badasswp <badasswpdev@gmail.com>
2023-12-03 04:42:23 +0100

    test: that Menu service registers Menu

--- tests/Services/MenuTest.php
+++ tests/Services/MenuTest.php
@@ -30,4 +30,28 @@ class MenuTest extends TestCase {
 
 		$this->assertConditionsMet();
 	}
+
+	public function test_register_menu() {
+		\WP_Mock::userFunction( '__' )
+			->twice()
+			->with( 'Xama', 'xama' )
+			->andReturn( 'Xama' );
+
+		\WP_Mock::userFunction( 'add_menu_page' )
+			->once()
+			->with(
+				'Xama',
+				'Xama',
+				'manage_options',
+				'xama',
+				false,
+				'dashicons-welcome-learn-more',
+				99
+			)
+			->andReturn( null );
+
+		$this->menu->register_menu();
+
+		$this->assertConditionsMet();
+	}
 }
