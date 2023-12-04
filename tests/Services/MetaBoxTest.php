35643bbfb4167d6ce34f2581d61c7677d32153de
badasswp <badasswpdev@gmail.com>
2023-12-04 20:43:28 +0100

    test: that MetaBox service registers meta boxes

--- tests/Services/MetaBoxTest.php
+++ tests/Services/MetaBoxTest.php
@@ -50,4 +50,112 @@ class MetaBoxTest extends TestCase {
 
 		$this->assertConditionsMet();
 	}
+
+	public function test_register_meta_boxes() {
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 4,
+				'return' => function ( $text, $domain = Settings::DOMAIN ) {
+					return $text;
+				},
+			]
+		);
+
+		// Loop 1
+		\WP_Mock::expectFilter(
+			'xama_metabox_options',
+			[
+				'name'      => 'xama_mb_answer',
+				'heading'   => 'Question Answer',
+				'post_type' => 'xama_question',
+				'position'  => '',
+				'priority'  => '',
+			]
+		);
+
+		\WP_Mock::userFunction( 'add_meta_box' )
+			->once()
+			->with(
+				'xama_mb_answer',
+				'Question Answer',
+				[ new Answer(), 'get_metabox_callback' ],
+				'xama_question',
+				'advanced',
+				'default'
+			);
+
+		// Loop 2
+		\WP_Mock::expectFilter(
+			'xama_metabox_options',
+			[
+				'name'      => 'xama_mb_options',
+				'heading'   => 'Question Options',
+				'post_type' => 'xama_question',
+				'position'  => '',
+				'priority'  => 'high',
+			]
+		);
+
+		\WP_Mock::userFunction( 'add_meta_box' )
+			->once()
+			->with(
+				'xama_mb_options',
+				'Question Options',
+				[ new Options(), 'get_metabox_callback' ],
+				'xama_question',
+				'advanced',
+				'high'
+			);
+
+		// Loop 3
+		\WP_Mock::expectFilter(
+			'xama_metabox_options',
+			[
+				'name'      => 'xama_mb_questions',
+				'heading'   => 'Quiz Questions',
+				'post_type' => 'xama_quiz',
+				'position'  => '',
+				'priority'  => 'high',
+			]
+		);
+
+		\WP_Mock::userFunction( 'add_meta_box' )
+			->once()
+			->with(
+				'xama_mb_questions',
+				'Quiz Questions',
+				[ new Questions(), 'get_metabox_callback' ],
+				'xama_quiz',
+				'advanced',
+				'high'
+			);
+
+		// Loop 4
+		\WP_Mock::expectFilter(
+			'xama_metabox_options',
+			[
+				'name'      => 'xama_mb_scores',
+				'heading'   => 'Quiz Scores',
+				'post_type' => 'xama_score',
+				'position'  => '',
+				'priority'  => 'high',
+			]
+		);
+
+		\WP_Mock::userFunction( 'add_meta_box' )
+			->once()
+			->with(
+				'xama_mb_scores',
+				'Quiz Scores',
+				[ new Scores(), 'get_metabox_callback' ],
+				'xama_score',
+				'advanced',
+				'high'
+			);
+
+		$this->metabox->register_meta_boxes();
+
+		$this->assertConditionsMet();
+	}
 }
