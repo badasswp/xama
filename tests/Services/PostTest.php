bcc6d2801f7c3aa1b79774c4647ab6c7a36acc77
badasswp <badasswpdev@gmail.com>
2023-12-08 23:48:11 +0100

    test: add tests to register Post Service

--- tests/Services/PostTest.php
+++ tests/Services/PostTest.php
@@ -35,4 +35,24 @@ class PostTest extends TestCase {
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
 	}
+
+	public function test_register() {
+		\WP_Mock::expectActionAdded( 'init', [ $this->post, 'register_post_types' ] );
+
+		\WP_Mock::expectFilterAdded( 'manage_xama_quiz_posts_columns', [ $this->post->objects[0], 'register_post_columns' ], 10, 2 );
+		\WP_Mock::expectFilterAdded( 'manage_xama_question_posts_columns', [ $this->post->objects[1], 'register_post_columns' ], 10, 2 );
+		\WP_Mock::expectFilterAdded( 'manage_xama_score_posts_columns', [ $this->post->objects[2], 'register_post_columns' ], 10, 2 );
+
+		\WP_Mock::expectActionAdded( 'manage_xama_quiz_posts_custom_column', [ $this->post->objects[0], 'register_post_column_data' ], 10, 2 );
+		\WP_Mock::expectActionAdded( 'manage_xama_question_posts_custom_column', [ $this->post->objects[1], 'register_post_column_data' ], 10, 2 );
+		\WP_Mock::expectActionAdded( 'manage_xama_score_posts_custom_column', [ $this->post->objects[2], 'register_post_column_data' ], 10, 2 );
+
+		\WP_Mock::expectActionAdded( 'publish_xama_quiz', [ $this->post->objects[0], 'save_post_type' ], 10, 2 );
+		\WP_Mock::expectActionAdded( 'publish_xama_question', [ $this->post->objects[1], 'save_post_type' ], 10, 2 );
+		\WP_Mock::expectActionAdded( 'publish_xama_score', [ $this->post->objects[2], 'save_post_type' ], 10, 2 );
+
+		$this->post->register();
+
+		$this->assertConditionsMet();
+	}
 }
