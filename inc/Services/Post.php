487ed7bd05f95ba1a4675b2f5365fcec6e7dc508
badasswp <badasswpdev@gmail.com>
2023-12-08 23:49:52 +0100

    refactor: refactor Post service to avoid multiple object creations

--- inc/Services/Post.php
+++ inc/Services/Post.php
@@ -24,11 +24,28 @@ class Post extends Service implements Registrable {
 	 * @since 1.0.0
 	 */
 	public function __construct() {
-		$this->post_types = [
+		$post_types = [
 			Quiz::class,
 			Question::class,
 			Score::class,
 		];
+
+		/**
+		 * Filter list of post types.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $post_types Post types.
+		 * @return array
+		 */
+		$post_types = (array) apply_filters( 'xama_post_types', $post_types );
+
+		foreach ( $post_types as $class ) {
+			if ( ! class_exists( $class ) ) {
+				throw new \LogicException( $class . ' does not exist.' );
+			}
+			$this->objects[] = new $class();
+		}
 	}
 
 	/**
@@ -41,10 +58,10 @@ class Post extends Service implements Registrable {
 	public function register(): void {
 		add_action( 'init', [ $this, 'register_post_types' ] );
 
-		foreach ( $this->post_types as $class ) {
-			add_filter( 'manage_' . $class::$name . '_posts_columns', [ new $class(), 'register_post_columns' ], 10, 2 );
-			add_action( 'manage_' . $class::$name . '_posts_custom_column', [ new $class(), 'register_post_column_data' ], 10, 2 );
-			add_action( 'publish_' . $class::$name, [ new $class(), 'save_post_type' ], 10, 2 );
+		foreach ( $this->objects as $object ) {
+			add_filter( 'manage_' . $object->get_name() . '_posts_columns', [ $object, 'register_post_columns' ], 10, 2 );
+			add_action( 'manage_' . $object->get_name() . '_posts_custom_column', [ $object, 'register_post_column_data' ], 10, 2 );
+			add_action( 'publish_' . $object->get_name(), [ $object, 'save_post_type' ], 10, 2 );
 		}
 	}
 
@@ -56,21 +73,8 @@ class Post extends Service implements Registrable {
 	 * @return void
 	 */
 	public function register_post_types() {
-		/**
-		 * Filter list of post types.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param array $post_types Post types.
-		 * @return array
-		 */
-		$this->post_types = (array) apply_filters( 'xama_post_types', $this->post_types );
-
-		foreach ( $this->post_types as $class ) {
-			if ( ! class_exists( $class ) ) {
-				throw new \LogicException( $class . ' does not exist.' );
-			}
-			( new $class() )->register_post_type();
+		foreach ( $this->objects as $object ) {
+			$object->register_post_type();
 		}
 	}
 
