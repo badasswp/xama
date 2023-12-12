2cf68f56d90b01cb64f34f1c559f641e73ae658a
badasswp <badasswpdev@gmail.com>
2023-12-12 20:33:05 +0100

    chore: build script for plugin

---
+++ bin/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Build Parameters
+PLUGIN_FILE="xama.php"
+PLUGIN_BUILD="build"
+PLUGIN_DIRS="dist inc languages"
+
+# Create the build directory
+if [ ! -d "$PLUGIN_BUILD" ]; then
+  	mkdir "$PLUGIN_BUILD"
+else
+  	rm -r "$PLUGIN_BUILD"
+	mkdir "$PLUGIN_BUILD"
+fi
+
+# Copy Plugin directories
+for dir in $PLUGIN_DIRS
+do
+  cp -r "$dir" "$PLUGIN_BUILD/$dir"
+done
+
+# Copy Plugin files
+cp "$PLUGIN_FILE" "$PLUGIN_BUILD/$PLUGIN_FILE"
+cp "readme.txt" "$PLUGIN_BUILD/readme.txt"
+cp "composer.json" "$PLUGIN_BUILD/composer.json"
+
+# Set up Vendor within Build
+cd "$PLUGIN_BUILD"
+composer install --no-dev
+rm "composer.json"
+rm "composer.lock"
+
+# Finish Build
+echo "Build complete!"
