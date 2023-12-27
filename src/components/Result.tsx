6dbee9e27858a56e45a10c03844dd08525b3eb7f
badasswp <badasswpdev@gmail.com>
2023-12-27 08:00:18 +0100

    feat: implement icons for Result component

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { stateProps } from '../store/reducers';
+import { IconUser, IconScore } from './Icons';
 
 /**
  * Result Component
@@ -21,9 +22,23 @@ const Result = () => {
           <div id="result">
             <div>
               <div>
-                <p>
-                  <strong>{ user.login } | Score: { percentage }%</strong>
-                </p>
+                <h1>{ percentage ? percentage : '0' }%</h1>
+              </div>
+              <div>
+                <header>
+                  <div>
+                    <span>
+                      <IconUser />
+                    </span>
+                    <strong>{ user.login }</strong>
+                  </div>
+                  <div>
+                    <span>
+                      <IconScore />
+                    </span>
+                    <strong>Score: { percentage ? percentage : '0' }%</strong>
+                  </div>
+                </header>
                 <h1>
                   <span>Congratulations!</span>
                 </h1>
