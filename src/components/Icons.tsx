5270689b03598038ed9617b66157addfe33d086b
badasswp <badasswpdev@gmail.com>
2023-12-27 07:59:38 +0100

    feat: add custom SVG icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,19 @@
+/**
+ * Icons Component
+ *
+ * This component returns SVG Icons.
+ *
+ * @returns {JSX.Element}  The rendered Icon components.
+ */
+
+export const IconUser = () => (
+  <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z" />
+  </svg>
+);
+
+export const IconScore = () => (
+  <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5h8m-1-3a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1m6 0v3H6V2m6 0h4a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4m0 9.464 2.025 1.965L12 9.571"/>
+  </svg>
+);
