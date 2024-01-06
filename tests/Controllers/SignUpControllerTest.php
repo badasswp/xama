0fc29b8d05345999bfbaf39a3f82a56fc4b7ae2c
badasswp <badasswpdev@gmail.com>
2024-01-06 15:20:29 +0100

    test: set up scaffolding for SignUpController tests

---
+++ tests/Controllers/SignUpControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Xama\Tests\Controllers;
+
+use Mockery;
+use stdClass;
+use Xama\Core\Settings;
+use WP_Mock\Tools\TestCase;
+use Xama\Controllers\SignUpController;
+
+/**
+ * @covers SignUpController
+ */
+class SignUpControllerTest extends TestCase {
+	private $controller;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+}
