c238d33c7e6ca6a4231806ff4825c766ef294096
badasswp <badasswpdev@gmail.com>
2024-01-06 15:22:10 +0100

    test: add test for reauthenticating users

--- tests/Controllers/SignUpControllerTest.php
+++ tests/Controllers/SignUpControllerTest.php
@@ -83,4 +83,53 @@ class SignUpControllerTest extends TestCase {
 		$this->assertTrue( empty( $_POST['http_error_msgs'] ) );
 		$this->assertConditionsMet();
 	}
+
+	public function test_reauth_user() {
+		$controller = Mockery::mock( SignUpController::class )->makePartial();
+
+		$controller->data = [
+			'xama_nonce'    => 'xama_nonce',
+			'xama_username' => 'john@doe.com',
+			'xama_password' => 'johndoe',
+		];
+
+		$user     = new stdClass();
+		$user->ID = 1;
+
+		\WP_Mock::userFunction( 'is_user_logged_in' )
+			->once()
+			->andReturn( true );
+
+		\WP_Mock::userFunction( 'wp_logout' )
+			->once()
+			->andReturn( null );
+
+		\WP_Mock::userFunction( 'get_user_by' )
+			->once()
+			->with( 'login', 'john@doe.com' )
+			->andReturn( $user );
+
+		\WP_Mock::userFunction( 'wp_set_current_user' )
+			->once()
+			->with( 1, 'john@doe.com' )
+			->andReturn( null );
+
+		\WP_Mock::userFunction( 'wp_set_auth_cookie' )
+			->once()
+			->with( 1 )
+			->andReturn( null );
+
+		\WP_Mock::userFunction( 'home_url' )
+			->once()
+			->andReturn( 'https://example.com' );
+
+		\WP_Mock::userFunction( 'wp_redirect' )
+			->once()
+			->with( 'https://example.com' )
+			->andReturn( null );
+
+		$controller->reauth_user( false );
+
+		$this->assertConditionsMet();
+	}
 }
